b44f6c8ddea86807141190688078ef2d2dc81306
jcayot <jules.cayot@icloud.com>
2024-11-22 13:33:54 +0200

    ouais

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -5,8 +5,8 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
-template <typename T, typename R>
-void	iter(T* array, const size_t len, R (*f)) {
+template <typename T, typename F>
+void	iter(T* array, const size_t len, F f) {
 	for (size_t i = 0; i < len; i++) {
 		f(array[i]);
 	}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,34 +7,22 @@
 
 #include "iter.hpp"
 
-class Awesome
-{
-	public:
-	Awesome( void ) : _n( 42 ) { return; }
-	int get( void ) const { return this->_n; }
-	private:
-	int _n;
-};
-
-std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
-{
-	o << rhs.get();
-	return o;
+void printInt(const int& x) {
+	std::cout << x << " ";
 }
 
-template< typename T >
-void print( T& x )
-{
-	std::cout << x << std::endl;
-	return;
+void printDouble(const double& x) {
+	std::cout << x << " ";
 }
 
 int main() {
-	int tab[] = { 0, 1, 2, 3, 4 };
-	Awesome tab2[5];
+	int intArr[] = {1, 2, 3, 4, 5};
+	::iter(intArr, 5, printInt);
+	std::cout << std::endl;
 
-	::iter( tab, 5, print<const int> );
-	::iter( tab2, 5, print<Awesome> );
+	const double doubleArr[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+	::iter(doubleArr, 5, printDouble);
+	std::cout << std::endl;
 
 	return 0;
 }
